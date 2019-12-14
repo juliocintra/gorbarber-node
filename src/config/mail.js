e1a1d014223a2a3107c7f7733f1f6b61ea5efc5d
Julio Cesar <julinn_cesar@hotmail.com>
2019-12-14 11:28:22 -0300

    Send mail finished

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '185f87008dc982',
+    pass: '294fe1eec23e8a',
+  },
+  default: {
+    from: 'Equipe gobarber <noreply@gobarber.com>',
+  },
+};
